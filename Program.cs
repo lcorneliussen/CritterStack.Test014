bce9db297be4abc372ca17555df5e9dcf95e0715
Lars Corneliussen <lc@talendos.com>
2024-03-25 22:32:13 +0100

    Simplified, still fails

--- Program.cs
+++ Program.cs
@@ -9,28 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.ApplyOaktonExtensions();
 builder.Host.UseWolverine();
 
-builder.Services.AddTransient<IService, HelloEndpointService>();
-
 var app = builder.Build();
 app.MapWolverineEndpoints();
-
 
 return await app.RunOaktonCommands(args);
 
 public class HelloEndpoint
 {
     [WolverineGet("/")]
-    public static string Get() => "Hello.";
-    
-    [WolverinePost("/")]
-    public static string Post(IService data) => "Hello.";
-}
-
-public interface IService
-{
-};
-
-public class HelloEndpointService : IService
-{
-    
+    public string Get() => "Hello.";
 }
